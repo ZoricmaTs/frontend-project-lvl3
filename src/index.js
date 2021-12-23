f0586c6692eb87cc20aa3e701e7318520cd64d9e
ZoricmaTs <tszzzch@gmail.com>
2021-12-24 01:42:51 +0700

    added input-form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import _ from 'lodash';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
